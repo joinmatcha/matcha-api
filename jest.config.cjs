9a6fc0fed1d0227965666c6afc60514f5af153dc
Ziwei.G <hijioki@qq.com>
2025-08-29 13:33:10 +0200

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src/tests'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src/tests"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
+  maxWorkers: 1,
+  testTimeout: 30000,
+  clearMocks: true,
+  detectOpenHandles: true,
 };
