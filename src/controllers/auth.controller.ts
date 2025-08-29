045e96aa0d42468d2f572dea9b909a0f8ca23450
Ziwei.G <hijioki@qq.com>
2025-08-29 12:29:00 +0200

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "@/models/User";
 
